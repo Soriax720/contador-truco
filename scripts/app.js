8648831f1677daac31f7dce73faed9f03b6d1819
Soriax720 <juliosoria744@gmail.com>
2023-01-11 18:30:31 -0300

    instanciado de objetos/constructor

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 import {Contador} from "./contador.js";
 
-const p1 = new Contador();
+const p1 = new Contador("Nosotros", document.getElementById("jugador1Container"));
+const p2 = new Contador("Ellos", document.getElementById("jugador2Container"));
 
-p1.agregar(2);
+p1.agregar(5);
 p1.reset();
-p1.agregar(1);
+p1.agregar();
+p2.agregar(4);
--- scripts/contador.js
+++ scripts/contador.js
@@ -2,14 +2,20 @@ export class Contador {
     numero = 0;
     nombre;
 
+    constructor(nombre, containerElement, cuentaInicial = 0){
+       this.nombre = nombre;
+       this.numero = cuentaInicial;
+       containerElement.querySelector("h2").innerText = nombre;
+    }
+
     agregar(cantidad = 1){
         this.numero += cantidad;
-        console.log(this.numero)
+        console.log(this.numero,this.nombre);
     }
 
     restar(cantidad = 1){
         this.numero = Math.max(0, this.numero-cantidad);
-        console.log(this.numero)
+        console.log(this.numero);
     }
     reset(){
         this.numero = 0;
