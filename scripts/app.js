5965dafa303cee6d41aa83944f3783798c0c709f
Soriax720 <juliosoria744@gmail.com>
2023-01-11 03:18:55 -0300

    contadores-OOP

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,6 @@ import {Contador} from "./contador.js";
 
 const p1 = new Contador();
 
+p1.agregar(2);
+p1.reset();
+p1.agregar(1);
--- scripts/contador.js
+++ scripts/contador.js
@@ -2,8 +2,16 @@ export class Contador {
     numero = 0;
     nombre;
 
-    agregar(){
-        this.numero ++;
+    agregar(cantidad = 1){
+        this.numero += cantidad;
         console.log(this.numero)
+    }
+
+    restar(cantidad = 1){
+        this.numero = Math.max(0, this.numero-cantidad);
+        console.log(this.numero)
+    }
+    reset(){
+        this.numero = 0;
     }
 }
